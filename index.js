e68a4a615317b9f9b42c2d3276e4bf4192ae4a22
Mhummad Mutahir <121317971+MRMutahir@users.noreply.github.com>
2023-03-24 10:14:42 +0500

    ok

---
+++ index.js
@@ -0,0 +1,42 @@
+// var text = 'bizwan'
+
+
+// text = text[0] = ('r') + text.slice(1)
+
+//  text = text.replace[0] = ('r') + text.slice(1)
+
+// text = text.substring[0] = ('r') + text.slice(1)
+
+//  text = replace = 'r' + text.slice(1)
+
+// text = 
+
+// console.log(text)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var nam = ['apple' , "banana" , "orange" , "pineapple"]
+
+nam = prompt  ("enter your favourite fruit name")
+
+if (nam) {
+    alert('fount it')
+}
+else (nam !==nam) {
+    alert("not found it")
+}
+
+console.log(nam)
